03bc151bbec67e93dbb2924cc1e429c38286aee0
Adrita2211 <105377016+Adrita2211@users.noreply.github.com>
2025-03-30 09:25:11 +0530

    Update ObjectDetectionFile.py

--- ObjectDetectionFile.py
+++ ObjectDetectionFile.py
@@ -21,15 +21,15 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 
 if uploaded_file:
     # Convert the uploaded file to a NumPy array
-    image = Image.open(uploaded_file).convert("RGB")
+    image = Image.open(uploaded_file)
     image_np = np.array(image)
 
     # Perform object detection using the YOLO model
     results = model(image_np)
 
     # Convert results to a format that can be displayed
-    result_img = np.squeeze(results.render())
-    #result_img = results[0].plot()
+    #result_img = np.squeeze(results.render())
+    result_img = results[0].plot()
     # Display the detected image
     st.image(result_img, caption="Detected Objects", use_column_width=True)
 
