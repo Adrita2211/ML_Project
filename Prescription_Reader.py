fe4928be94da3fa9c564583db572fe12a72fe18c
Adrita2211 <105377016+Adrita2211@users.noreply.github.com>
2025-05-01 18:11:28 +0530

    Update Prescription_Reader.py

--- Prescription_Reader.py
+++ Prescription_Reader.py
@@ -3,7 +3,7 @@ from google import genai
 import re
 
 # Set your Google AI API key
-API_KEY = st.secrets["api_key"]  # Store your API key securely in Streamlit secrets
+API_KEY = st.secrets["GOOGLE_API_KEY"]  # Store your API key securely in Streamlit secrets
 client = genai.Client(api_key=API_KEY)
 
 # Define the prompt
